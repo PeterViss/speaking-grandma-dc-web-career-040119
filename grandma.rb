8daec2a5aae3f02aa578a19ae5754a326d6492ca
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 22:48:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 
-  elseif "#{phrase}".upcase
+  elseif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
 
   else

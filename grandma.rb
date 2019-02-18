97364757493b5502f8a87ef480b6c2bd5c40db04
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 22:42:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,11 +5,11 @@ def speak_to_grandma(phrase)
   elseif "#{phrase.uppercase}"
     puts "NO, NOT SINCE 1938!"
 
-  else 
+  else
     puts "HUH?! SPEAK UP, SONNY!"
 
 
-  
+
   end # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with

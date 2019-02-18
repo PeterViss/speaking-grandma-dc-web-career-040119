d6bd0d98edff3b0e08ba426b0bfcb4a429353f72
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 23:02:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(speak)
   if speak == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
 
-  elseif speak == speak.upcase
+  else-if speak == speak.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"

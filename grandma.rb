6b0281c05383891f3d3f3b87673ff46b6453724f
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 22:53:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,10 @@ def speak_to_grandma(phrase)
 
   elseif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
 
   else
     return "HUH?! SPEAK UP, SONNY!"
